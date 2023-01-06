74fdbbb46c6e76da4a1ada796d04969deb773fea
Alvaro <alvaroreig@gmail.com>
2023-01-06 20:54:14 +0100

    overdue and today working

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import re
-from datetime import datetime
+from datetime import datetime,date
 
 from flask import Flask
 from flask import render_template
 from dotenv import load_dotenv
 import os
 import logging
+import locale
 
 from todoist_api_python.api import TodoistAPI
 
@@ -25,23 +26,56 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
+    locale.setlocale(locale.LC_TIME, os.getenv('LOCALE'))
     api = TodoistAPI(os.getenv('TODOIST_API_KEY'))
     filter = os.getenv('TODOIST_FILTER')
-    tasks = api.get_tasks(filter=filter)
-    logging.debug(api)
+    today = datetime.today()
+    logging.debug("today: " + str(today))
 
+    try:
+        tasks = api.get_tasks(filter=filter)
+    except Exception as e:
+        try:
+            logging.error(e)
+            tasks = api.get_tasks(filter=filter)
+        except Exception as e:
+            logging.error(e)
+            tasks = []
     
-    #tasks = api.get_tasks()
-    logging.debug('Recovered ' + str(len(tasks)) + ' task(s)')
+    logging.info('Recovered ' + str(len(tasks)) + ' task(s)')
+    #logging.debug(tasks)
+
+    overdue = []
+    today_list = []
+
+    for task in tasks:
+        logging.debug('task content: ' + task.content)
+        logging.debug('task datetine ' + str(task.due.datetime))
+        try:
+            if (task.due.datetime is not None):
+                parsed_date=datetime.strptime(task.due.datetime,"%Y-%m-%dT%H:%M:%SZ")
+            else:
+                parsed_date=datetime.strptime(task.due.date,"%Y-%m-%d")
+        except Exception as e:
+            logging.error("Parsing error,task " + task.content)
+        
+        logging.debug('task parsed due datetime: ' + str(parsed_date))
+
+        if (parsed_date.day == today.day and parsed_date.month == today.month and parsed_date.year == today.year):
+            today_list.append(task.content)
+        elif (parsed_date < today):
+            overdue.append(task.content)
+
+
+    logging.debug("overdue tasks: " + str(len(overdue)))
+    logging.debug(overdue)
+
+    logging.debug("today tasks: " + str(len(today_list)))
+    logging.debug(today_list)
 
     return render_template(
         "index.html",
-        date=datetime.now()
-    )
-
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "index.html",
-        date=datetime.now()
+        date=today.strftime('%A,%d %B %Y'),
+        overdue=overdue,
+        today = today_list
     )
