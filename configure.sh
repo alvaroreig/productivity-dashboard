9d02dfc44fe9593c315a95b1f88555fc4c893f03
alvaroreig <alvaroreig@gmail.com>
2024-12-29 16:01:41 +0100

    renaming config script

--- configure.sh
+++ configure.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
+
 # Set up env
 python3 -m venv .venv
 source .venv/bin/activate
 
 # Install requirements
 python3 -m pip install -r requirements.txt
-
-# Run app
-flask run --host 0.0.0.0
